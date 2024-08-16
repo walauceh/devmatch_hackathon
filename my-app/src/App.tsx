5d3b067671ed4bd9fce4ccae28c5996e5e5816b3
Baygorr <145186196+Baygorr@users.noreply.github.com>
2024-08-16 09:50:08 +0800

    hehe

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleNav = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="body-pd" className={isExpanded ? 'body-pd' : ''}>
+      <div className="logo-container">
+        <img src="logo512.png" alt="Logo" className="logo-image" />
+      </div>
+
+      <div className={`l-navbar ${isExpanded ? 'expander' : ''}`} id="navbar">
+        <nav className="nav">
+          <div>
+            <div className="nav__brand">
+              <i className="bx bx-menu nav__toggle" id="nav-toggle" onClick={toggleNav}></i>
+            </div>
+            <div className="nav__list">
+              <a href="#" className="nav__link active">
+              <i className='bx bx-wallet'></i>
+                <span className="nav__name">My Wallet</span>
+              </a>
+              <a href="#" className="nav__link">
+              <i className='bx bxs-credit-card-alt'></i>
+                <span className="nav__name">View Card</span>
+              </a>
+
+              <a href="#" className="nav__link">
+              <i className ='bx bxs-file'></i>
+                <span className="nav__name">Document</span>
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
